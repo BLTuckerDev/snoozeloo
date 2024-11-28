fbebeb427a48dd4697cb4d5b0105ed2c3543077b
Brett <bretttucker@gmail.com>
2024-11-27 18:24:32 -0700

    Update theme and fonts

--- app/src/main/java/dev/bltucker/snoozeloo/common/theme/Theme.kt
+++ app/src/main/java/dev/bltucker/snoozeloo/common/theme/Theme.kt
@@ -1,46 +1,28 @@
 package dev.bltucker.snoozeloo.common.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = SnoozelooBlue,
+    secondary = SnoozelooBluePale,
+    tertiary = SnoozelooBlack,
+    background = SnoozelooWhite,
+    surface = SnoozelooWhite,
+    onPrimary = SnoozelooWhite,
+    onSecondary = SnoozelooBlack,
+    onTertiary = SnoozelooWhite,
+    onBackground = SnoozelooBlack,
+    onSurface = SnoozelooBlack,
 )
 
 @Composable
 fun SnoozelooTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = LightColorScheme,
         typography = Typography,
         content = content
     )
